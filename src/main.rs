908d31396d55e85858ed7f0d11526af24353974d
Alexandre Chêne <kooparse@protonmail.com>
2019-11-13 21:23:17 -0500

    Add tracker script

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
 mod controllers;
 mod db;
 
-use actix_web::{web, App, HttpServer};
+use actix_cors::Cors;
+use actix_web::{web, App, HttpServer, Responder};
 use controllers::health;
 use db::Db;
 use dotenv;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Data {
+    id: String,
+    pid: Option<String>,
+    href: String,
+    website_id: String,
+    hostname: String,
+    origin: String,
+    pathname: String,
+}
+
+fn tracker(params: web::Query<Data>) -> impl Responder {
+    dbg!(params);
+    dbg!("Hello World")
+}
 
 /// All routes are here.
 fn main() -> std::io::Result<()> {
@@ -16,10 +34,13 @@ fn main() -> std::io::Result<()> {
     let server_port = dotenv::var("SERVER_PORT").unwrap_or("3333".to_string());
     let bind_address = format!("{}:{}", server_address, server_port);
 
-    HttpServer::new(|| {
-        App::new()
-            .data(Db::new())
-            .service(web::resource("/health").to(health))
+    HttpServer::new(move || {
+        App::new().data(Db::new()).wrap(Cors::default()).service(
+            // TODO: Should be "api".
+            web::scope("/")
+                .route("/tracker", web::get().to(tracker))
+                .route("/health", web::get().to(health)),
+        )
     })
     .bind(bind_address)?
     .run()
