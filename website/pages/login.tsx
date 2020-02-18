c22e1d0cfb1a486b2c14d2213089fa84f752f99a
Alexandre Chêne <kooparse@protonmail.com>
2020-02-18 17:11:18 +0100

    Use chakra-ui for login page

--- website/pages/login.tsx
+++ website/pages/login.tsx
@@ -1,50 +1,73 @@
-import React, { Component } from "react";
+import React, { Component, useContext } from "react";
 import Router from "next/router";
-import FormLayout from "../components/FormLayout";
-import { Form, FormField, TextInput, Box, Button } from "grommet";
+import { useFormik } from "formik";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Button
+} from "@chakra-ui/core";
 import api, { setToken } from "../utils/api";
 import { UserContext } from "../utils/context";
+import FormLayout from "../components/FormLayout";
 
-class Login extends Component {
-  static contextType = UserContext;
+export default () => {
+  const ctx = useContext(UserContext);
+  const formik = useFormik({
+    initialValues: { email: "", password: "" },
+    onSubmit: async (values, actions) => {
+      try {
+        const { data } = await api.post("/login", values);
+        console.log(data);
 
-  onSubmit = async (form) => {
-    try {
-      const { data } = await api.post("/login", form.value);
-      console.log(data);
-      setToken(data.token);
-      this.context.setUser(data.user);
-      Router.push("/");
-    } catch (e) {
-      console.error(e);
+        setToken(data.token);
+        ctx.setUser(data.user);
+        Router.push("/");
+      } catch (e) {
+        console.error(e);
+        actions.setSubmitting(false);
+      }
     }
-  };
+  });
 
-  render() {
-    return (
-      <FormLayout>
-        <Form onSubmit={this.onSubmit}>
-          <FormField
-            label="Email"
-            name="email"
+  return (
+    <FormLayout>
+      <form onSubmit={formik.handleSubmit}>
+        <FormControl p={4}>
+          <FormLabel htmlFor="email">Email address</FormLabel>
+          <Input
             type="email"
-            placeholder="test@protonmail.com"
-            required
+            id="email"
+            aria-describedby="email-helper-text"
+            value={formik.values.email}
+            onChange={formik.handleChange}
           />
-          <FormField
-            label="Password"
-            name="password"
-            type="password"
-            placeholder="Your secret password"
-            required
-          />
-          <Box direction="row" margin={{ top: "medium" }}>
-            <Button type="submit" label="Submit" primary />
-          </Box>
-        </Form>
-      </FormLayout>
-    );
-  }
-}
+        </FormControl>
 
-export default Login;
+        <FormControl p={4}>
+          <FormLabel htmlFor="password">Your password</FormLabel>
+          <Input
+            type="password"
+            id="password"
+            name="password"
+            aria-describedby="password-helper-text"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+          />
+        </FormControl>
+
+        <Button
+          ml={4}
+          mt={4}
+          variantColor="teal"
+          isLoading={formik.isSubmitting}
+          type="submit"
+        >
+          Login
+        </Button>
+      </form>
+    </FormLayout>
+  );
+};
