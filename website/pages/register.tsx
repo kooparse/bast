9f7af195385fef2d55f5f79b5bbc85981bc607f2
Alexandre Chêne <kooparse@protonmail.com>
2020-02-18 17:05:32 +0100

    Use react hooks

--- website/pages/register.tsx
+++ website/pages/register.tsx
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, { Component, useContext } from "react";
 import Router from "next/router";
-import { Formik, Form, Field } from "formik";
+import { useFormik } from "formik";
 import {
   FormControl,
   FormLabel,
@@ -13,90 +13,76 @@ import api, { setToken } from "../utils/api";
 import { UserContext } from "../utils/context";
 import FormLayout from "../components/FormLayout";
 
-class Register extends Component {
-  static contextType = UserContext;
+export default () => {
+  const ctx = useContext(UserContext);
+  const formik = useFormik({
+    initialValues: { email: "", password: "" },
+    onSubmit: async (values, actions) => {
+      try {
+        // First register new user.
+        await api.post("/register", values);
+        // Then logged new user.
+        const { data } = await api.post("/login", values);
+        // Store all info in localStorage + set axios header.
+        setToken(data.token);
+        ctx.setUser(data.user);
+        Router.push("/");
+      } catch (e) {
+        console.error(e);
 
-  render() {
-    return (
-      <FormLayout>
-        <Formik
-          initialValues={{ email: "", password: "" }}
-          onSubmit={async (values, actions) => {
-            try {
-              // First register new user.
-              await api.post("/register", values);
-              // Then logged new user.
-              const { data } = await api.post("/login", values);
-              // Store all info in localStorage + set axios header.
-              setToken(data.token);
-              this.context.setUser(data.user);
-              Router.push("/");
-            } catch (e) {
-              console.error(e);
+        // Cleanup.
+        actions.setSubmitting(false);
+      }
+    }
+  });
 
-              // Cleanup.
-              actions.setSubmitting(false);
-            }
-          }}
+  return (
+    <FormLayout>
+      <form onSubmit={formik.handleSubmit}>
+        <FormControl
+          p={4}
+          isRequired
+          isInvalid={formik.errors.email && formik.touched.email}
         >
-          {props => (
-            <form onSubmit={props.handleSubmit}>
-              <Field name="email">
-                {({ field, form }) => (
-                  <FormControl
-                    p={4}
-                    isRequired
-                    isInvalid={form.errors.email && form.touched.email}
-                  >
-                    <FormLabel htmlFor="email">Email address</FormLabel>
-                    <Input
-                      type="email"
-                      id="email"
-                      aria-describedby="email-helper-text"
-                      {...field}
-                    />
-                    <FormHelperText id="email-helper-text">
-                      We'll never share your email.
-                    </FormHelperText>
-                  </FormControl>
-                )}
-              </Field>
+          <FormLabel htmlFor="email">Email address</FormLabel>
+          <Input
+            type="email"
+            id="email"
+            aria-describedby="email-helper-text"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+          />
+          <FormHelperText id="email-helper-text">
+            We'll never share your email.
+          </FormHelperText>
+        </FormControl>
 
-              <Field name="password">
-                {({ field, form }) => (
-                  <FormControl
-                    isRequired
-                    p={4}
-                    isInvalid={form.errors.password && form.touched.password}
-                  >
-                    <FormLabel htmlFor="password">
-                      Your secret password
-                    </FormLabel>
-                    <Input
-                      type="password"
-                      id="password"
-                      aria-describedby="password-helper-text"
-                      {...field}
-                    />
-                  </FormControl>
-                )}
-              </Field>
+        <FormControl
+          isRequired
+          p={4}
+          isInvalid={formik.errors.password && formik.touched.password}
+        >
+          <FormLabel htmlFor="password">Your secret password</FormLabel>
+          <Input
+            type="password"
+            id="password"
+            name="password"
+            aria-describedby="password-helper-text"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+          />
+        </FormControl>
 
-              <Button
-                ml={4}
-                mt={4}
-                variantColor="teal"
-                isLoading={props.isSubmitting}
-                type="submit"
-              >
-                Submit
-              </Button>
-            </form>
-          )}
-        </Formik>
-      </FormLayout>
-    );
-  }
-}
-
-export default Register;
+        <Button
+          ml={4}
+          mt={4}
+          variantColor="teal"
+          isLoading={formik.isSubmitting}
+          type="submit"
+        >
+          Submit
+        </Button>
+      </form>
+    </FormLayout>
+  );
+};
