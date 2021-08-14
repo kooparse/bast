481cd71eca25f3865990061dd4771c9fc4f07a10
Alexandre Chêne <kooparse@protonmail.com>
2021-08-14 11:46:40 +0200

    Fix eslint error

--- website/.eslintrc.js
+++ website/.eslintrc.js
@@ -7,6 +7,5 @@ module.exports = {
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
-    "prettier/@typescript-eslint"
   ]
 };
